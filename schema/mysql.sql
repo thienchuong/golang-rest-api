8b2c12c2811e33f655b69d9d58c0ff62314dc725
tonix <thienchuong004@gmail.com>
2024-08-03 20:57:01 +0700

    add schema

---
+++ schema/mysql.sql
@@ -0,0 +1,6 @@
+CREATE TABLE books(
+  id int AUTO_INCREMENT PRIMARY KEY,
+  title varchar(255) NOT NULL,
+  author varchar(255) NOT NULL,
+  year int NOT NULL
+);
---
+++ schema/postgres.sql
@@ -0,0 +1,6 @@
+CREATE TABLE books(
+  id serial PRIMARY KEY,
+  title text NOT NULL,
+  author text NOT NULL,
+  year int NOT NULL
+);
