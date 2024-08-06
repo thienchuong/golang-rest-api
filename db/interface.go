60cc61c2554fe5fd91f3d81c97be2c6f3d4334a7
tonix <thienchuong004@gmail.com>
2024-08-06 21:41:49 +0700

    add package db and model

---
+++ db/interface.go
@@ -0,0 +1,11 @@
+package db
+
+import "github.com/thienchuong/golang-rest-api/models"
+
+type Database interface {
+	GetAllBooks() ([]models.Book, error)
+	GetBookByID(id int) (models.Book, error)
+	CreateBook(book models.Book) (models.Book, error)
+	UpdateBook(id int, book models.Book) (models.Book, error)
+	DeleteBook(id int) error
+}
